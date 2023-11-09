455af58eaa0f14e7381c7a171c03a9b387a87327
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-09 00:22:28 -0800

    commit

--- benchmarks/inference/mii/postprocess_results.py
+++ benchmarks/inference/mii/postprocess_results.py
@@ -43,7 +43,8 @@ def parse_args():
 def get_tokenizer():
     global tokenizer
     if tokenizer is None:
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+        #tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+        tokenizer = AutoTokenizer.from_pretrained("/models/llama-2-70b-chat-hf")
     return tokenizer    
 
 
