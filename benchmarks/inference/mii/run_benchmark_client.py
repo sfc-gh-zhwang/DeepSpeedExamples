5ffcdc3c44bf683036cbeacae19fd2b050cf9c9c
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-10 10:51:53 -0800

    commit

--- benchmarks/inference/mii/run_benchmark_client.py
+++ benchmarks/inference/mii/run_benchmark_client.py
@@ -112,7 +112,7 @@ def call_mii(client, input_tokens, max_new_tokens, stream):
             streaming_fn=callback)
     else:
         result = client.generate(
-            input_tokens, max_new_tokens=max_new_tokens, postprocess_config=postprocess_config)
+            input_tokens, max_new_tokens=max_new_tokens)
         output_tokens = result.response[0]
 
     return ResponseDetails(
