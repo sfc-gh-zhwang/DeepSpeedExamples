72b42609a946462278e8405ed8f628a2d8cf91a4
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-11 21:30:43 -0800

    commit

--- benchmarks/inference/mii/utils.py
+++ benchmarks/inference/mii/utils.py
@@ -1,6 +1,7 @@
 import csv
 from scipy import stats
 import numpy as np
+import math
 
 
 def calculate_mean(data, confidence_level=0.95):
@@ -27,10 +28,31 @@ def calculate_mean(data, confidence_level=0.95):
     return f'{mean:.4f}[{lb:.4f}, {up:.4f}]'
 
 
-def get_prompts(n):
+# def get_prompts(n):
+#     prompts = []
+#     with open('prompts/prompts2048.csv', newline='') as file:
+#         reader = csv.reader(file)
+#         for row in reader:
+#             prompts.append(row[0])
+#     return prompts[:n]
+
+
+def shrink_sentence(s, r):
+    words = s.split()
+    n = math.ceil(len(words) * r)
+    return ' '.join(words[:n])
+
+
+# r=0.3775 -> mean tokens 1024
+# r=0.179 -> mean tokens 512
+# r=0.079 -> mean tokens 256
+def get_prompts(n, r=1.0):
     prompts = []
-    with open('prompts/prompts2048.csv', newline='') as file:
+    with open('prompts/arxiv.csv', newline='') as file:
         reader = csv.reader(file)
         for row in reader:
-            prompts.append(row[0])
-    return prompts[:n]
+            prompt = f'''<s>[INST]<<SYS>>
+    Please summarize the text that is given. Return just the summary and no additional conversational dialog such as ""Sure, here is the summary of the text:"".
+    <</SYS>> {shrink_sentence(row[0], r)}[/INST]'''
+            prompts.append(prompt)
+    return prompts
