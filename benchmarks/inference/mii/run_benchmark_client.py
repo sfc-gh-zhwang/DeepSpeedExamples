e304f2366548df7921d4cecd673926ee2b36af6e
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-11 21:41:58 -0800

    commit

--- benchmarks/inference/mii/run_benchmark_client.py
+++ benchmarks/inference/mii/run_benchmark_client.py
@@ -80,8 +80,10 @@ def call_mii_bulk(client, query_queue, result_queue):
     latency = []
     for i in tqdm(range(10)):
         start_time = time.time()
+        print(prompts)
         result = client.generate(
             prompts, max_new_tokens=512, top_p=1.0, temperature=1.0)
+        print(result)
         end_time = time.time()
         latency.append(end_time-start_time)
         print(calculate_mean(latency))
